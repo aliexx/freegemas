48b8abc85606aa5a63de9d54651ce46a73201bed
Alexey Kuzovlev <aliexx@mail.ru>
2016-05-31 14:24:23 +0500

    Initial commit

---
+++ android/src/com/siondream/freegemas/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.siondream.freegemas;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.siondream.freegemas.Freegemas;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Freegemas(), config);
+	}
+}
---
+++ desktop/src/com/siondream/freegemas/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.siondream.freegemas.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.siondream.freegemas.Freegemas;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Freegemas(), config);
+	}
+}
